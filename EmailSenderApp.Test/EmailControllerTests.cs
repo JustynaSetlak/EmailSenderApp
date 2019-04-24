1560493303a531295cf82d1375b9542cbdfc3599
JustynaSetlak <justa96.s1@gmail.com>
2019-04-24 20:48:26 +0200

    integration test sample

---
+++ EmailSenderApp.Test/EmailControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using EmailSenderApp.Api;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Text;
+using System.Threading.Tasks;
+using EmailSender.Dtos.Requests;
+using Xunit;
+using Newtonsoft.Json;
+using System.Net.Http;
+using FluentAssertions;
+
+namespace EmailSenderApp.Tests
+{
+    public class EmailControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        private const string API_BASE = "/api/message";
+
+        public EmailControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Test()
+        {
+            //Arrange
+            var client = _factory.CreateClient();
+            var message = new EmailMessageDto
+            {
+                Body = "Hi",
+                Title = "Welcome",
+                To = "justa96.s1@gmail.com"
+            };
+
+            //Act
+            var stringContent = new StringContent(JsonConvert.SerializeObject(message), Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(API_BASE, stringContent);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Accepted);
+        }
+
+    }
+}
